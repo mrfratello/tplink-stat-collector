03d8f0e014b369b81c0089123c96565dd0c78e65
John Syomochkin <info@syomochkin.xyz>
2019-02-16 12:01:18 +0300

    proxy local backend for frontend

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,11 @@ module.exports = {
         contentBase: './resources',
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'http://127.0.0.1:3000',
                 changeOrigin: true
             }
         },
+        port: 5000,
         hot: true
     },
     plugins: [
